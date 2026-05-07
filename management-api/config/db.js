b3937e3bb1e3e236e0663ba3ed4e681d7bb768cf
Thinh <thinhnguyen672007@gmail.com>
2026-05-07 15:53:07 +0700

    Build a web for tracking schedule and students

---
+++ management-api/config/db.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql2/promise');
+
+require('dotenv').config();
+
+const pool = mysql.createPool({
+    host : process.env.DB_HOST,
+    port : process.env.DB_PORT,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    name : process.env.DB_NAME,
+    waitForConnections : true,
+    connectionLimit : 10,
+    enableKeepAlive : true
+})
+
+async function testConnection() {
+    try{
+         const conn = await pool.getConnection();
+         console.log("Database connection")
+         conn.release();
+    } catch (error) {
+        console.log("Fail to connect database!", error.message);
+        process.exit(1);    
+    }
+}
+
+module.exports = pool;
